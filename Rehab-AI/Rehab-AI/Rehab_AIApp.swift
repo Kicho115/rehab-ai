a0f128c0185dfbb081bf4ba39c9ac4439c187a40
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-04-02 19:16:08 -0700

    Initial commit

---
+++ Rehab-AI/Rehab-AI/Rehab_AIApp.swift
@@ -0,0 +1,20 @@
+//
+//  Rehab_AIApp.swift
+//  Rehab-AI
+//
+//  Created by Oscar Angulo on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Rehab_AIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
