0a0b703fe9b8d6655ade7af0ff7e73ee5a1cc1f6
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-04-04 19:30:22 -0700

    Add basic dashboard layout

---
+++ Rehab-AI/Rehab-AI/Views/Components/RecoveryProgress.swift
@@ -0,0 +1,15 @@
+//
+//  DailyProgress.swift
+//  Rehab-AI
+//
+//  Created by Oscar Angulo on 4/4/25.
+//
+
+import SwiftUI
+
+struct RecoveryProgress: View {
+    var body: some View {
+        Text("Daily Progress")
+            
+    }
+}
